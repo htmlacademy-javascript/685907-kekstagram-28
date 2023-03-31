4a53452a88ad0afb6aee5d817d461bfb48a23ab4
Noskov Ivan <bopogold@gmail.com>
2023-03-31 11:54:57 +0300

    Добавляет модуль работы с сервером

--- js/util.js
+++ js/util.js
@@ -21,4 +21,25 @@ function createRandomIdFromRangeGenerator(min, max) {
   };
 }
 
-export {getRandomInteger,createRandomIdFromRangeGenerator};
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '18px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'tomato';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, 5000);
+};
+
+export {getRandomInteger,createRandomIdFromRangeGenerator, showAlert};
